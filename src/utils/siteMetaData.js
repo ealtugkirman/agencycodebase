a559851246ef0ccef27ae4e7cf29801b7e611a95
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2024-02-22 17:47:25 +0300

    favicon

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,26 +1,26 @@
 // please update required information
 const siteMetadata = {
-  title: "DigitalVoyage.Agency",
-  author: "DigitalVoyage.Agency",
-  headerTitle: "this.altcoinvoyage.com",
-  name: "google-adsense-account",
+  title: "DigitalVoyage.agency",
+  author: "DigitalVoyage.agency",
+  headerTitle: "digitalvoyage.agency",
+  // name: "google-adsense-account",
   // content: "ca-pub-8265654758847402",
 
   description:
-    "Welcome to AltcoinVoyage, your timeless portal to the realm of alternative cryptocurrencies",
-  language: "en-us",
-  theme: "dark", // system, dark or light
-  siteUrl: "https://altcoinvoyage.com/", // your website URL
+    "Digital Voyage, Türkiye merkezli yeni nesil çözümler sunan dijital danışmanlık ve web geliştirme ajansıdır.",
+  language: "tr",
+  theme: "light",
+  siteUrl: "https://digitalvoyage.agency/", // your website URL
   siteLogo: "/logo.png",
   socialBanner: "/social-banner.png", // add social banner in the public folder
   // email: 'codebucks27@gmail.com',
   // github: 'https://github.com/codebucks27',
-  twitter: "https://twitter.com/AltcoinVoyage",
+  // twitter: "https://twitter.com/AltcoinVoyage",
   // facebook: 'https://facebook.com',
   // youtube: 'https://youtube.com/codebucks',
   // linkedin: 'https://www.linkedin.com/in/codebucks/',
   // dribbble: 'https://www.dribbble.com',
-  locale: "en-US",
+  locale: "tr",
 };
 
 module.exports = siteMetadata;
