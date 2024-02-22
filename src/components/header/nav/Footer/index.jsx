fe28ac7eea3d4924efc5313cbdde3bd151b9132a
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2024-02-22 15:00:49 +0300

    isItUpdated

--- src/components/header/nav/Footer/index.jsx
+++ src/components/header/nav/Footer/index.jsx
@@ -1,11 +1,19 @@
-import styles from './style.module.scss';
-import { translate } from '../../anim';
-import { motion } from 'framer-motion';
+import styles from "./style.module.scss";
+import { translate } from "../../anim";
+import { motion } from "framer-motion";
 
 export default function Footer() {
-    return (
-        <div className={styles.footer}>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. At assumenda pariatur amet similique dignissimos qui odit ut, consectetur rem exercitationem. Illo asperiores reprehenderit unde obcaecati qui, voluptas debitis deleniti porro voluptates quam sit voluptatibus, minima aliquam? Nobis culpa modi fugiat itaque fuga.</p>
-        </div>
-    )
+  return (
+    <div className={styles.footer}>
+      <p>
+        2024'te dünya genelinde 452,961,207 web sitesi var. Bunların arasında
+        kaybolmamak için digital voyage ile websitesinizi herkesi hayranlık
+        içinde bırakacak şekilde inşa edelim. Websitesini tasarımınızı,
+        hikayenizi ve kişiliğinizi yansıtacak şekilde özel olarak hazırlayarak,
+        diğerlerinden ayıralım. Birbirin kopyası olan websitelerinden
+        sıkıldıgınızı biliyoruz. Bu yüzden Digital Voyage ile yolculugunuzu
+        başlatalım
+      </p>
+    </div>
+  );
 }
