0a977eea1db38e87564c3ad7415efcd62ea6dc1c
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2024-03-23 22:26:29 +0300

    links

--- src/components/Blog/VoyageBlog.jsx
+++ src/components/Blog/VoyageBlog.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const VoyageBlog = () => {
   return (
-    <div className="flex-col justify-center border-black border-t-2 border-b-2 flex py-8">
-      <h1 className="text-8xl text-center">VOYAGE BLOG</h1>
+    <div className="flex-col justify-center border-black border-t-2 border-b-2 flex py-2 md:py-6 lg:py-8">
+      <h1 className="text-4xl lg:text-6xl xl:text-8xl text-center">VOYAGE BLOG</h1>
     </div>
   );
 };
